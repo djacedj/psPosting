64bc8c8253453b06721bba203fa5c930a9d25a4b
alvaro prudencio <aprudencio@bodaclick.com>
2014-05-05 18:13:24 +0200

    Controlling number of images

--- js/controllers.js
+++ js/controllers.js
@@ -26,13 +26,29 @@ psPostingControllers.controller('MainCtrl', ['$scope', 'Brand', '$fileUploader',
         uploader.filters.push(function(item /*{File|HTMLInputElement}*/) {
             var type = uploader.isHTML5 ? item.type : '/' + item.value.slice(item.value.lastIndexOf('.') + 1);
             type = '|' + type.toLowerCase().slice(type.lastIndexOf('/') + 1) + '|';
-            return '|jpg|png|jpeg|gif|'.indexOf(type) !== -1;
+            if (!('|jpg|png|jpeg|gif|'.indexOf(type) !== -1)) {
+                alert("El archivo debe ser una imagen");
+                return false;
+            }
+            if (!$scope.name || !$scope.brandSelected || !$scope.bodyArea) {
+                alert("Por favor rellena los datos de la imagen");
+                return false;
+            }
+            console.info(uploader.getNotUploadedItems().length);
+            return true;
         });
+
+        uploader.bind('afteraddingfile', function (event, item) {
+            item.formData ["name"] = $scope.name;
+            item.formData ["brand"] = $scope.brandSelected;
+            item.formData ["bodyArea"] = $scope.bodyArea;
+        });
+
 
         // REGISTER HANDLERS
 
-        uploader.bind('afteraddingfile', function (event, item) {
-            console.info('After adding a file', item);
+        /*uploader.bind('afteraddingfile', function (event, item) {
+            console.info('Acabo de subir un archivo', item);
         });
 
         uploader.bind('whenaddingfilefailed', function (event, item) {
@@ -40,7 +56,7 @@ psPostingControllers.controller('MainCtrl', ['$scope', 'Brand', '$fileUploader',
         });
 
         uploader.bind('afteraddingall', function (event, items) {
-            console.info('After adding all files', items);
+            console.info('Acabo de subir todos los archivos', items);
         });
 
         uploader.bind('beforeupload', function (event, item) {
@@ -73,6 +89,6 @@ psPostingControllers.controller('MainCtrl', ['$scope', 'Brand', '$fileUploader',
 
         uploader.bind('completeall', function (event, items) {
             console.info('Complete all', items);
-        });
+        });*/
     }
 ]);
